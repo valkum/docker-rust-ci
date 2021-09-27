25a3de09cc25f12754d41915d4856b7e621ec764
Rudi Floren <rudi.floren@gmail.com>
2021-09-27 22:28:05 +0200

    Update to rust 1.55

--- README.md
+++ README.md
@@ -3,9 +3,9 @@ Docker stable container with batteries included.
 
 # Supported tags and respective Dockerfile links
 
-* [`1.50.0-buster`, `1.50.0`, `buster`, `latest`](https://github.com/valkum/docker-rust-ci/blob/1.50.0/buster/Dockerfile)
-* [`1.50.0-slim-buster`, `1.50.0-slim`, `slim-buster`, `slim`](https://github.com/valkum/docker-rust-ci/blob/1.50.0/buster/slim/Dockerfile)
-* [`1.50.0-alpine3.12`, `1.50.0-alpine`, `alpine3.12`, `alpine`](https://github.com/valkum/docker-rust-ci/blob/1.50.0/alpine3.12/Dockerfile)
+* [`1.55.0-buster`, `1.55.0`, `buster`, `latest`](https://github.com/valkum/docker-rust-ci/blob/1.55.0/buster/Dockerfile)
+* [`1.55.0-slim-buster`, `1.55.0-slim`, `slim-buster`, `slim`](https://github.com/valkum/docker-rust-ci/blob/1.55.0/buster/slim/Dockerfile)
+* [`1.55.0-alpine3.14`, `1.55.0-alpine`, `alpine3.14`, `alpine`](https://github.com/valkum/docker-rust-ci/blob/1.55.0/alpine3.14/Dockerfile)
 
 # Additional software installed
 * sccache
--- build-all.sh
+++ build-all.sh
@@ -1,3 +1,24 @@
-docker build buster
-docker build buster/slim
-docker build alpine3.12
+docker build buster -t valkum/docker-rust-ci:1.55.0-buster
+docker image tag valkum/docker-rust-ci:1.55.0-buster valkum/docker-rust-ci:buster
+docker image tag valkum/docker-rust-ci:1.55.0-buster valkum/docker-rust-ci:1.55.0
+docker image tag valkum/docker-rust-ci:1.55.0-buster valkum/docker-rust-ci:latest
+docker push valkum/docker-rust-ci:1.55.0-buster
+docker push valkum/docker-rust-ci:buster
+docker push valkum/docker-rust-ci:1.55.0
+docker push valkum/docker-rust-ci:latest
+docker build buster/slim -t valkum/docker-rust-ci:1.55.0-slim-buster
+docker image tag valkum/docker-rust-ci:1.55.0-slim-buster valkum/docker-rust-ci:1.55.0-slim
+docker image tag valkum/docker-rust-ci:1.55.0-slim-buster valkum/docker-rust-ci:slim-buster
+docker image tag valkum/docker-rust-ci:1.55.0-slim-buster valkum/docker-rust-ci:slim
+docker push valkum/docker-rust-ci:1.55.0-slim-buster
+docker push valkum/docker-rust-ci:1.55.0-slim
+docker push valkum/docker-rust-ci:slim-buster
+docker push valkum/docker-rust-ci:slim
+docker build alpine3.14 -t valkum/docker-rust-ci:1.55.0-alpine3.14
+docker image tag valkum/docker-rust-ci:1.55.0-alpine3.14 valkum/docker-rust-ci:1.55.0-alpine
+docker image tag valkum/docker-rust-ci:1.55.0-alpine3.14 valkum/docker-rust-ci:alpine3.14
+docker image tag valkum/docker-rust-ci:1.55.0-alpine3.14 valkum/docker-rust-ci:alpine
+docker push valkum/docker-rust-ci:1.55.0-alpine3.14
+docker push valkum/docker-rust-ci:1.55.0-alpine
+docker push valkum/docker-rust-ci:alpine3.14
+docker push valkum/docker-rust-ci:alpine
